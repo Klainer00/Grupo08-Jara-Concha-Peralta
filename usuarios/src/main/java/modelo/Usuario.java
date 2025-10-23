1e93bbf75ac7433b665affc098124c747aca8ddf
Klainer00 <166978145+Klainer00@users.noreply.github.com>
2025-10-23 03:11:17 -0300

    a

--- usuarios/src/main/java/modelo/Usuario.java
+++ usuarios/src/main/java/modelo/Usuario.java
@@ -1,5 +1,30 @@
 package modelo;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
+@Entity
+@Table(name = "usuarios") 
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    @Column(unique = true, nullable = false) 
+    private String correo;
+
+    private String contraseña;
+
+    private String direccion;
+    
+    private String telefono;
 
 }
