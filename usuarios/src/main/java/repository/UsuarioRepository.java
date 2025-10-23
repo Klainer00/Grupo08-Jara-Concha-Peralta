8386341a35471bab3a6ceee6d3fb9847a014a250
Klainer00 <166978145+Klainer00@users.noreply.github.com>
2025-10-23 03:11:17 -0300

    a

--- usuarios/src/main/java/repository/UsuarioRepository.java
+++ usuarios/src/main/java/repository/UsuarioRepository.java
@@ -1,5 +1,13 @@
 package repository;
 
-public class UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import modelo.Usuario;
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario , Long> {
+    Optional<Usuario> findByCorreo(String correo);
+    Optional<Usuario> findById(Long id);
 }
---
+++ usuarios/src/main/java/service/UsuarioService.java
@@ -0,0 +1,60 @@
+package service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import modelo.Usuario;
+import repository.UsuarioRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> obtenerTodosLosUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+
+    public Optional<Usuario> obtenerUsuarioPorId(Long id) {
+        return usuarioRepository.findById(id);
+    }
+
+    public Usuario crearUsuario(Usuario usuario) {
+
+        if (usuarioRepository.findByCorreo(usuario.getCorreo()).isPresent()) {
+            
+            throw new RuntimeException("El correo ya está en uso");
+        }
+        
+        return usuarioRepository.save(usuario);
+    }
+
+    public Optional<Usuario> actualizarUsuario(Long id, Usuario usuarioDetalles) {
+        Optional<Usuario> usuarioOpt = usuarioRepository.findById(id);
+
+        if (usuarioOpt.isEmpty()) {
+            return Optional.empty();
+        }
+
+        Usuario usuarioExistente = usuarioOpt.get();
+        usuarioExistente.setNombre(usuarioDetalles.getNombre());
+        usuarioExistente.setCorreo(usuarioDetalles.getCorreo());
+        usuarioExistente.setContraseña(usuarioDetalles.getContraseña());
+        usuarioExistente.setDireccion(usuarioDetalles.getDireccion());
+        usuarioExistente.setTelefono(usuarioDetalles.getTelefono());
+        return Optional.of(usuarioRepository.save(usuarioExistente));
+    }
+
+
+    public boolean borrarUsuario(Long id) {
+        if (usuarioRepository.existsById(id)) {
+            usuarioRepository.deleteById(id);
+            return true;
+        }
+        return false; 
+    }
+}
